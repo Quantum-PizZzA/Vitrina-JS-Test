22a3f9193992eac1efc80320024e20e9db3311db
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-05-26 09:50:11 +0300

    Update barcode.js

--- javascript/barcode.js
+++ javascript/barcode.js
@@ -1,16 +1,15 @@
 function barcode(value) {
-  let eanCode1 = "4607144738088";
-      let eanCode = "97qqq";
+      let eanCode = value;
 
       let up = 0;
       let down = 0;
       Stringup = "";
       Stringdown  = "";
 
-      console.log("eanCode.length=", eanCode.length);
+      //console.log("eanCode.length=", eanCode.length);
 
       for (let i = eanCode.length - 1; i >= 0; i--) {
-        console.log("i=", i, "up =", up,"down=", down);
+        //console.log("i=", i, "up =", up,"down=", down);
         if (i % 2 == 0) {
           Stringup = String(eanCode[i])  + " + "+ Stringup;
           up = up + Number(eanCode[i]);
@@ -19,12 +18,12 @@ function barcode(value) {
           down = down + Number(eanCode[i]);
         }
       }
-      console.log("верх =",Stringup,"=", up);
-      console.log("низ    =",Stringdown,"=", down*3);
+      //console.log("верх =",Stringup,"=", up);
+      //console.log("низ    =",Stringdown,"=", down*3);
       sum=down*3+up;
-      console.log("sum=",sum );
+      //console.log("sum=",sum );
       if ((sum % 10) == 0) {
         console.log("Хороший штрихкод");
       } else {
         console.log("Плохой штрихкод");
-      }
+      }}
