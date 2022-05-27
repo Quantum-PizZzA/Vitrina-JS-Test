fa4b0d7a7521de79d0dcd98853ef57d99f442044
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-05-27 14:28:23 +0300

    Revert "Update barcode.js"
    This reverts commit 2cbb57c86c141ebb9ee319864b207b07a4f2626c.

--- javascript/barcode.js
+++ javascript/barcode.js
@@ -8,26 +8,18 @@ function barcode(value) {
 
   //console.log("eanCode.length=", eanCode.length);
 
-  console.log(eanCode.length);
-
-  if (eanCode.length % 2 == 0) {
-    eanCode = "0" + eanCode;
-    console.log(eanCode);
-  }
-
-  if (eanCode.length)
-    for (let i = eanCode.length - 1; i >= 0; i--) {
-      //console.log("i=", i, "up =", up,"down=", down);
-      if (i % 2 == 0) {
-        Stringup = String(eanCode[i]) + " + " + Stringup;
-        up = up + Number(eanCode[i]);
-      } else {
-        Stringdown = String(eanCode[i]) + " + " + Stringdown;
-        down = down + Number(eanCode[i]);
-      }
+  for (let i = eanCode.length - 1; i >= 0; i--) {
+    //console.log("i=", i, "up =", up,"down=", down);
+    if (i % 2 == 0) {
+      Stringup = String(eanCode[i]) + " + " + Stringup;
+      up = up + Number(eanCode[i]);
+    } else {
+      Stringdown = String(eanCode[i]) + " + " + Stringdown;
+      down = down + Number(eanCode[i]);
     }
-  console.log("верх =", Stringup, "=", up);
-  console.log("низ    =", Stringdown, "=", down * 3);
+  }
+  //console.log("верх =",Stringup,"=", up);
+  //console.log("низ    =",Stringdown,"=", down*3);
   sum = down * 3 + up;
   //console.log("sum=",sum );
   if (sum % 10 == 0) {
