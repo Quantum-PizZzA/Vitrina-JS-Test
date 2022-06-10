3f3123d0070d4d1a23c8e91a62d6848165252641
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-06-10 09:42:29 +0300

    QQQ

---
+++ VITRINA/javascript/table.js
@@ -0,0 +1,23 @@
+function createTable() {
+  var row = parseInt(document.getElementById("row").value); //row - ряд
+  var col = parseInt(document.getElementById("col").value); //col - столбец
+
+  var tablestart = "<table id=myTable border=1>";
+  var tableend = "</table>";
+  var trstart = "<tr bgcolor=#ff9966>"; //начало ряда
+  var trend = "</tr>";
+  var tdstart = "<td>"; //начало строки
+  var tdend = "</td>"; //конец  строки
+  var data = "data in cell";
+  var str1 = tablestart + trstart + tdstart + data + tdend + trend + tableend;
+  document.write(tablestart);
+
+  for (var r = 0; r < row; r++) {
+    //row - ряд
+    document.write(trstart); //row - ряд
+    for (var c = 0; c < col; c++) {
+      //col - столбец
+      document.write(tdstart + " r= " + r + " c= " + c + tdend); //col - столбец
+    }
+  }
+}
