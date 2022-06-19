d9e95f2c2deb7875997bb7cc12e2ab379231768c
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-06-19 19:36:45 +0300

    Update ean_test.js

--- cmd_javascript/ean_test.js
+++ cmd_javascript/ean_test.js
@@ -8,7 +8,7 @@ function checkEAN(value) {
 
   if (eanCode.length % 2 == 0) {
     eanCode = "0" + eanCode;
-    WScript.Echo("eanCode: ", eanCode);
+    //WScript.Echo("eanCode: ", eanCode);
   }
   //WScript.Echo("eanCode.length: ", eanCode.length);
 
@@ -26,12 +26,16 @@ function checkEAN(value) {
   //WScript.Echo("sum: ", sum, "down: ", down, "up: ", up);
   return sum % 10 === 0;
 }
-var m = 4607548752482;
+var s = 4607548752482 - 1;
+var f = 4607548752482 + 1;
 
-for (var i = m - 1; i <= m + 1; i++) {
-  WScript.Echo("  ");
+// var s = 1;
+// var f = 1000000000;
+
+for (var i = s; i <= f; i++) {
+  //WScript.Echo("  ");
   if (checkEAN(i) == 0) {
-    // WScript.Echo(i, " no ean ", checkEAN(i));
+    WScript.Echo(i, " no ean ", checkEAN(i));
   } else {
     WScript.Echo(i, " ean!!! ", checkEAN(i));
   }
