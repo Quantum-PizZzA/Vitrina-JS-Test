a3e9d0be662f39a41bea4b74c84d22bc51231e40
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-06-28 10:00:58 +0300

    Update script.js

--- +SQLite-IndexedDB-WebSQL-Node-JS/IndexedDB/script.js
+++ +SQLite-IndexedDB-WebSQL-Node-JS/IndexedDB/script.js
@@ -1,6 +1,6 @@
 // Это работает на всех устройствах / браузерах и использует IndexedDBShim в качестве последнего запасного варианта
 n = 1;
-console.log(n++, "Проверка браузера");
+console.log("1. Проверка браузера");
 const indexedDB =
   window.indexedDB ||
   window.mozIndexedDB ||
@@ -9,18 +9,18 @@ const indexedDB =
   window.shimIndexedDB;
 
 // Открыта (или создана) база данных
-console.log(n++, "Открытие создание базы");
+console.log("2. Открытие создание базы");
 const request = indexedDB.open("CarsDatabase", 1);
 
 request.onerror = function (event) {
-  console.log(n++, "Обработка ошибки");
+  console.log("3. Обработка ошибки");
   console.error("An error occurred with IndexedDB");
   console.error(event);
 };
 
 // Создана схема при создании и обновлении версии
 request.onupgradeneeded = function () {
-  console.log(n++, "Создание базы");
+  console.log("4. Создание базы");
   const db = request.result;
   const store = db.createObjectStore("cars", { keyPath: "id" });
   store.createIndex("cars_colour", ["colour"], { unique: false });
@@ -30,7 +30,7 @@ request.onupgradeneeded = function () {
 };
 
 request.onsuccess = function () {
-  console.log(n++, "База данных успешно открыта");
+  console.log("5. База данных успешно открыта");
 
   const db = request.result;
   const transaction = db.transaction("cars", "readwrite");
@@ -40,7 +40,7 @@ request.onsuccess = function () {
   const makeModelIndex = store.index("colour_and_make");
 
   // Добавленые некоторые данные
-  console.log(n++, "Запись данных");
+  console.log("6. Запись данных");
   store.put({ id: 1, colour: "Красный", make: "Toyota" });
   store.put({ id: 2, colour: "Красный", make: "Kia" });
   store.put({ id: 3, colour: "Синий", make: "Honda" });
@@ -48,28 +48,30 @@ request.onsuccess = function () {
   store.put({ id: 5, colour: "Золото", make: "BMW" });
   store.put({ id: 6, colour: "Серебро", make: "Mercedes" });
 
+  console.log("Запросы");
+
   // Запрос по идентификатору
   const idQuery = store.get(4);
   idQuery.onsuccess = function () {
-    console.log("запрос идентификатора", idQuery.result);
+    console.log("7. запрос идентификатора", idQuery.result);
   };
 
   // Запрос цвета Красный
   const colourQuery = colourIndex.getAll(["Красный"]);
   colourQuery.onsuccess = function () {
-    console.log("запрос цвета", colourQuery.result);
+    console.log("8. запрос цвета", colourQuery.result);
   };
 
   // Запрос "Синий", "Honda"
   const colourMakeQuery = makeModelIndex.get(["Синий", "Honda"]);
   colourMakeQuery.onsuccess = function () {
-    console.log("запрос на создание цвета", colourMakeQuery.result);
+    console.log("9. запрос на создание цвета", colourMakeQuery.result);
   };
 
   // Удалить id 1
   const deleteCar = store.delete(1);
   deleteCar.onsuccess = function () {
-    console.log("Красная Toyota была удалена");
+    console.log("10. Красная Toyota была удалена");
   };
 
   // Удалить синие машины
@@ -78,7 +80,7 @@ request.onsuccess = function () {
     const deleteCar = store.delete(redCarKey.result);
 
     deleteCar.onsuccess = function () {
-      console.log("машина была удалена");
+      console.log("11 машина была удалена");
     };
   };
 
