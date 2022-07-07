0d33c9d016031c8cbcc0e6447599cf1c38da2f3e
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-07-07 15:34:51 +0300

    words

--- +Only-HTML-Firebase-JS/index_LocalStorage_firebace/test.js
+++ +Only-HTML-Firebase-JS/index_LocalStorage_firebace/test.js
@@ -1,18 +1,72 @@
 function Test() {
-  for (i = 0; i < 10000; i++) {
-    console.log(i);
+  for (i = 0; i < 100; i++) {
     maxinn = 999999999999;
     mininn = 1000000000;
     maxPrice = 8000;
     minPrice = 10;
-    J = {
-      EAN: "14601234567890",
-      INN: Math.floor(Math.random() * (maxinn - mininn + 1)) + mininn,
-      Price: Math.floor(Math.random() * (maxPrice - minPrice + 10)) + minPrice,
-      Name: "Печенье",
-      Date: Date.now()
-      // Date2: new Date()
-    };
-    localStorage.setItem(Date.now() + i, JSON.stringify(J));
+
+    const words = [
+      "Кошка",
+      "Собака",
+      "Трактор",
+      "Компьютер",
+      "Стеллаж",
+      "Ручка",
+      "Нож",
+      "Автомат",
+      "Танк",
+      "Дверь",
+      "Телефон",
+      "Монитор",
+      "Сайт",
+      "USB провод",
+      "Коробка",
+      "Автомобиль",
+      "Наушники",
+      "Кружка",
+      "Обруч",
+      "Гиря."
+    ];
+
+    const word = words[Math.floor(Math.random() * (words.length + 1))];
+
+    // const word = words[randomNumber(0, words.length - 1)];
+
+    if (checkEAN(i) == 0) {
+      // WScript.Echo(i, " no ean ", checkEAN(i));
+    } else {
+      // WScript.Echo(i, " ean!!! ", checkEAN(i));
+      J = {
+        EAN: i,
+        INN: Math.floor(Math.random() * (maxinn - mininn + 1)) + mininn,
+        Price:
+          Math.floor(Math.random() * (maxPrice - minPrice + 10)) + minPrice,
+        Name: word,
+        Date: Date.now()
+        // Date2: new Date()
+      };
+    }
+
+    localStorage.setItem(Date.now() + localStorage.length, JSON.stringify(J));
   }
+}
+
+function checkEAN(value) {
+  var eanCode = String(value);
+  var up = 0;
+  var down = 0;
+
+  if (eanCode.length % 2 == 0) {
+    eanCode = "0" + eanCode;
+  }
+
+  for (var i = eanCode.length - 1; i >= 0; i--) {
+    if (i % 2 == 0) {
+      up = up + Number(eanCode.charAt(i));
+    } else {
+      down = down + Number(eanCode.charAt(i));
+    }
+  }
+  var sum = down * 3 + up;
+  return sum % 10 === 0;
 }
