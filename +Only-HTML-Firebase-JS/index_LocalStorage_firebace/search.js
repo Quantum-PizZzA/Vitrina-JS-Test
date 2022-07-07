e6757098ce210d85a7867ba364c50b86007328ac
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-07-07 16:55:50 +0300

    Update search.js

--- +Only-HTML-Firebase-JS/index_LocalStorage_firebace/search.js
+++ +Only-HTML-Firebase-JS/index_LocalStorage_firebace/search.js
@@ -1,11 +1,13 @@
 let disSearch = " \n";
 
+// console.log(/'\d*'/.test("abcde"));
+// console.log(/'\d*'/.test("abcde"));
+
 function Search() {
   valueSearch = document.getElementById("boxsearch").value;
   disSearch = "\t" + "Ничего не найдено";
   for (let i = 0; i < localStorage.length; i++) {
     if (localStorage.key(i) == valueSearch) {
-      console.log(localStorage.key(i), " =? ", valueSearch);
       let key = localStorage.key(i);
 
       disSearch =
