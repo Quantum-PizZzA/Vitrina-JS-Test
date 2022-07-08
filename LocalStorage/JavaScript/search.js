7f61e898079ae48fa14030a7d2813d209abcbc81
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-07-08 15:59:56 +0300

    LS

--- LocalStorage/JavaScript/search.js
+++ LocalStorage/JavaScript/search.js
@@ -6,7 +6,20 @@ function Search() {
   console.log("function Search");
 
   numbSearch = 0;
-  disSearch = "";
+  disSearch = 
+  "\t" +
+  "№" +
+  "\t" +
+  "Ключ" +
+  "\t" +
+  "EAN" +
+  "\t" +
+  "INN" +
+  "\t" +
+  "Цена" +
+  "\t" +
+  "Название" +
+  "\n";
 
   for (let i = 0; i < localStorage.length && numbSearch < 20; i++) {
     let array = localStorage.getItem(localStorage.key(i));
