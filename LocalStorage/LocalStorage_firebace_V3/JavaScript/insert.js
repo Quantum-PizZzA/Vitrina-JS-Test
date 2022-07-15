d819fdca3120f1d1d6029968e5648a0e911743c2
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-07-15 14:56:27 +0300

    Revert "test"
    This reverts commit 003aa7b9b8668b41b1c366409a4c3c56180fb7bc.

--- LocalStorage/LocalStorage_firebace_V3/JavaScript/insert.js
+++ LocalStorage/LocalStorage_firebace_V3/JavaScript/insert.js
@@ -1,22 +1,21 @@
 function Insert() {
   box = document.getElementById("box").value;
 
-  // let arr4j = box.split(" ");
+  let arr = box.split(" ");
 
-  // var ean = arr4j[0];
-  // var inn = arr4j[1];
-  // var price = arr4j[2];
-  // var name = arr4j[3];
+  var ean = arr[0];
+  var inn = arr[1];
+  var price = arr[2];
+  var name = arr[3];
 
-  // valueJSON = {
-  //   EAN: ean,
-  //   INN: inn,
-  //   Price: price,
-  //   Name: name,
-  //   Date: Date.now()
-  //   // Date2: new Date()
-  // };
+  valueJSON = {
+    EAN: ean,
+    INN: inn,
+    Price: price,
+    Name: name,
+    Date: Date.now()
+    // Date2: new Date()
+  };
 
-  // localStorage.setItem(Date.now(), JSON.stringify(valueJSON));
-  localStorage.setItem(Date.now(), box);
+  localStorage.setItem(Date.now(), JSON.stringify(valueJSON));
 }
