cc9d16d026d1f4ea9b8bb4d2dc05c583b95b68dd
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-07-18 16:49:43 +0300

    Версия: 0.4.7

--- LocalStorage/LocalStorage_firebace_V4/JavaScript/ReadFile.js
+++ LocalStorage/LocalStorage_firebace_V4/JavaScript/ReadFile.js
@@ -6,11 +6,22 @@ function ReadFileJSON() {
   reader.readAsText(file);
 
   reader.onload = function () {
+    // console.log(reader.result);
     obj = JSON.parse(reader.result);
+    // console.log(obj);
 
     for (key in obj) {
-      // console.log(key, obj[key]);
-      localStorage.setItem(key, JSON.stringify(obj[key]));
+      console.log(key, obj[key]);
+      localStorage.setItem(
+        key,
+        obj[key].EAN +
+          " " +
+          obj[key].INN +
+          " " +
+          obj[key].Price +
+          " " +
+          obj[key].Name
+      );
     }
   };
 }
