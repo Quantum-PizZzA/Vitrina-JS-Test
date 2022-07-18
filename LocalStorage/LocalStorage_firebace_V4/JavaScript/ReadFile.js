6ce01f821f5179cda05429f3de2aeb52ebfb2c02
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-07-18 11:14:44 +0300

    read write

--- LocalStorage/LocalStorage_firebace_V4/JavaScript/ReadFile.js
+++ LocalStorage/LocalStorage_firebace_V4/JavaScript/ReadFile.js
@@ -5,8 +5,63 @@ function ReadFile() {
 
   reader.readAsText(file);
 
+  Arr = [];
+  lines = [];
+  linesValue = [];
+
   reader.onload = function () {
-    testJSON = JSON.parse(reader.result);
-    console.log(`Добавленный файл: \n${testJSON}`);
+    let lines = reader.result.split(";");
+
+    // console.log(lines);
+
+    for (i = 0; i < lines.length; i++) {
+      Arr = lines[i].split(":");
+
+      linesValue.key = Arr[0];
+      linesValue.ean = Arr[1];
+      linesValue.inn = Arr[2];
+      linesValue.price = Arr[3];
+      linesValue.name = Arr[4];
+
+      if (
+        linesValue.key != "" &&
+        linesValue.key != " " &&
+        linesValue.key != "\n" &&
+        linesValue.key != "\t"
+      ) {
+        localStorage.setItem(
+          linesValue.key,
+          linesValue.ean +
+            " " +
+            linesValue.inn +
+            " " +
+            linesValue.price +
+            " " +
+            linesValue.name
+        );
+        console.log("Загружено:", linesValue);
+      }
+    }
+  };
+}
+
+function ReadFileJSON() {
+  let file = document.getElementById("file").files[0];
+
+  let reader = new FileReader();
+
+  reader.readAsText(file);
+
+  reader.onload = function () {
+    obj = JSON.parse(reader.result);
+
+    // reader.result = reader.result.replace(/(\r\n|\n|\r)/gm, "");
+
+    console.log(reader.result);
+    console.log(reader);
+    console.log(obj[1658125448586]);
+    console.log(obj[1658125448586].INN);
+
+    // localStorage.setItem("key", obj);
   };
 }
--- LocalStorage/LocalStorage_firebace_V4/JavaScript/WriteFile.js
+++ LocalStorage/LocalStorage_firebace_V4/JavaScript/WriteFile.js
@@ -1,31 +1,59 @@
 function WriteFile() {
   arrayFile = "";
   for (let i = 0; i < localStorage.length; i++) {
+    renferfind = localStorage.getItem(localStorage.key(i));
+    let arr = renferfind.split(" ");
+
+    let ean = arr[0];
+    let inn = arr[1];
+    let price = arr[2];
+    let name = arr[3];
+
     arrayFile +=
-      '"' +
       localStorage.key(i) +
-      '":' +
-      localStorage.getItem(localStorage.key(i)) +
-      ",";
-
-    console.log(localStorage.getItem(localStorage.key(i)));
-
-    // arrayFile = JSON.str(
-    //   localStorage.key(i),
-    //   localStorage.getItem(localStorage.key(i))
-    // );
-
-    // '"' +
-    // localStorage.key(i) +
-    // '":' +
-    // localStorage.getItem(localStorage.key(i)) +
-    // ",";
+      ":" +
+      ean +
+      ":" +
+      inn +
+      ":" +
+      price +
+      ":" +
+      name +
+      "; \n";
   }
 
   save =
-    "<a download='localStorage.json' href='data:application/octet-stream;UTF-8, + arrayFile + >download localStorage.json</a>";
-  // "<a download='localStorage.json' href='data:application/octet-stream;UTF-8,{" +
-  // arrayFile +
-  // "} '>download localStorage.json</a>";
+    "<a download='localStorage.txt' href='data:application/octet-stream;UTF-8," +
+    arrayFile +
+    "'>download localStorage.txt</a>";
+  document.write(save);
+}
+
+function WriteFileJSON() {
+  arrayFile = "";
+  for (let i = 0; i < localStorage.length; i++) {
+    renferfind = localStorage.getItem(localStorage.key(i));
+    let arr = renferfind.split(" ");
+
+    let ean = arr[0];
+    let inn = arr[1];
+    let price = arr[2];
+    let name = arr[3];
+
+    valueJSON = {
+      EAN: ean,
+      INN: inn,
+      Price: price,
+      Name: name
+    };
+
+    arrayFile +=
+      '"' + localStorage.key(i) + '":' + JSON.stringify(valueJSON) + ",";
+  }
+
+  save =
+    "<a download='localStorage.json' href='data:application/octet-stream;UTF-8,{" +
+    arrayFile +
+    "} '>download localStorage.json</a>";
   document.write(save);
 }
