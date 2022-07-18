6444e00e5f911510ddef7d382531f95c1b42b892
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-07-18 15:45:46 +0300

    for (key in obj) {

--- LocalStorage/LocalStorage_firebace_V4/JavaScript/ReadFile.js
+++ LocalStorage/LocalStorage_firebace_V4/JavaScript/ReadFile.js
@@ -8,56 +8,17 @@ function ReadFileJSON() {
   reader.onload = function () {
     obj = JSON.parse(reader.result);
 
-    // reader.result = reader.result.replace(/(\r\n|\n|\r)/gm, "");
-
     console.log(reader.result);
     console.log(reader);
-    // console.log(obj[1658125448586]);
+    console.log(obj);
+
+    for (key in obj) {
+      console.log(key, obj[key]);
+      /* ... делать что-то с obj[key] ... */
+    }
+
     // console.log(obj[1658125448586].INN);
 
     // localStorage.setItem("key", obj);
   };
 }
-
-// function ReadFile() {
-//   let file = document.getElementById("file").files[0];
-
-//   let reader = new FileReader();
-
-//   reader.readAsText(file);
-
-//   Arr = [];
-//   lines = [];
-//   linesValue = [];
-
-//   reader.onload = function () {
-//     // console.log(reader);
-
-//     let lines = reader.result.split("\n");
-
-//     for (i = 0; i < lines.length; i++) {
-//       Arr = lines[i].split(";");
-
-//       linesValue.key = Arr[0];
-//       linesValue.ean = Arr[1];
-//       linesValue.inn = Arr[2];
-//       linesValue.price = Arr[3];
-//       linesValue.name = Arr[4];
-
-//       if (String(Number(linesValue.key)) != "NaN") {
-//         console.log("Запись", Number(linesValue.key));
-
-//         localStorage.setItem(
-//           Number(linesValue.key),
-//           linesValue.ean +
-//             " " +
-//             linesValue.inn +
-//             " " +
-//             linesValue.price +
-//             " " +
-//             linesValue.name
-//         );
-//       }
-//     }
-//   };
-// }
