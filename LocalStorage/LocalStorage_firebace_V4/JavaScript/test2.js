bc0f654a56b2b577e94dd3a39ce539ffd7c36fc9
Isanin Sergey <92434473+Quantum-PizZzA@users.noreply.github.com>
2022-07-18 16:58:37 +0300

    1

---
+++ LocalStorage/LocalStorage_firebace_V4/JavaScript/test2.js
@@ -0,0 +1,9 @@
+function Test2_0() {
+  valueJSONtest = {
+    EAN: i + 10,
+    INN: Math.floor(Math.random() * (maxinn - mininn + 1)) + mininn,
+    Price: Math.floor(Math.random() * (maxPrice - minPrice + 10)) + minPrice,
+    Name: word,
+    Date: Date.now()
+  };
+}
